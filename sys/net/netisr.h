1948b87ad08f75d055ad7427f31fd95408c44dff
Henning Matyschok <henning.matyschok@stud.fh-flensburg.de>
2018-10-04 13:50:17 +0200

    Licence-header removed.

--- sys/net/netisr.h
+++ sys/net/netisr.h
@@ -29,31 +29,6 @@
  *
  * $FreeBSD: releng/11.1/sys/net/netisr.h 301270 2016-06-03 13:57:10Z bz $
  */
-/*
- * Copyright (c) 2018 Henning Matyschok
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
 #ifndef _NET_NETISR_H_
 #define _NET_NETISR_H_
 
@@ -83,7 +58,7 @@
 #define	NETISR_EPAIR	8		/* if_epair(4) */
 #define	NETISR_IP_DIRECT	9	/* direct-dispatch IPv4 */
 #define	NETISR_IPV6_DIRECT	10	/* direct-dispatch IPv6 */
-#define NETISR_CAN 	11 		/* AF_CAN input queue */
+#define NETISR_CAN 	11 		/* AF_CAN input queue */ 
 
 /*
  * Protocol ordering and affinity policy constants.  See the detailed
