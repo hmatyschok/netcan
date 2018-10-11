b24dc8081058c915c3d66dc5706f7c016458e8b0
Henning Matyschok <henning.matyschok@stud.fh-flensburg.de>
2018-10-11 17:26:33 +0200

    Misc. cleanup.

--- sys/net/if_slc.c
+++ sys/net/if_slc.c
@@ -114,6 +114,7 @@ static int 	slc_modevent(module_t, int, void *);
 static struct if_clone *slc_cloner;
 static const char slc_name[] = "slc";
 
+
 static int
 slc_clone_create(struct if_clone *ifc, int unit, caddr_t data)
 {
@@ -166,31 +167,6 @@ slc_clone_destroy(struct ifnet *ifp)
 	TAILQ_REMOVE(&slc_list, slc, slc_list);
 	mtx_unlock(&slc_list_mtx);
 	slc_destroy(slc);
-}
-
-static void
-slc_destroy(struct slc_softc *slc)
-{
-	struct ifnet *ifp;
-	struct tty *tp;
-	
-	ifp = SLC2IFP(slc);
-	ifp->if_drv_flags &= ~IFF_DRV_RUNNING;
-	ifp->if_flags &= ~IFF_UP;
-
-	can_ifdetach(ifp);
-	if_free(ifp);
-	
-	/* detach hook, if any and flush queue */
-	if ((tp = slc->slc_tp) != NULL) {
-		tty_lock(tp);
-		ttyhook_unregister(tp);
-	}
-	IF_DRAIN(&slc->slc_outq);
-	mtx_destroy(&slc->slc_outq.ifq_mtx);
-	
-	mtx_destroy(&slc->slc_mtx);
-	free(slc, M_SLC);
 }
 
 static int
@@ -318,6 +294,222 @@ slc_start_locked(struct ifnet *ifp)
 	ifp->if_drv_flags &= ~IFF_DRV_OACTIVE;
 }
 
+/*
+ * Process an ioctl(2) request.
+ */
+/* ARGSUSED */
+static int
+slc_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
+{
+	struct ifreq *ifr = (struct ifreq *)data;
+	struct ifdrv *ifd = (struct ifdrv *)data;
+	struct slc_softc *slc;
+	int error;
+
+	if ((slc = ifp->if_softc) == NULL) {
+		error = EINVAL;
+		goto out;
+	} else
+		error = 0;
+
+	switch (cmd) {
+	case SIOCSIFADDR:
+		ifp->if_flags |= IFF_UP;
+		ifp->if_drv_flags |= IFF_DRV_RUNNING;
+		break;
+	case SIOCADDMULTI:
+	case SIOCDELMULTI:
+		if (ifr == NULL) {
+			error = EAFNOSUPPORT;		/* XXX */
+			break;
+		}
+		
+		switch (ifr->ifr_addr.sa_family) {
+#ifdef CAN
+		case AF_CAN:
+			break;
+#endif 	/* CAN */
+		default:
+			error = EAFNOSUPPORT;
+			break;
+		}
+		break;
+	case SIOCGDRVSPEC:
+	case SIOCSDRVSPEC:
+	
+		switch (ifd->ifd_cmd) {
+		case SLCSTTY:
+			if (ifd->ifd_len != sizeof(int))
+				error = EINVAL;
+			else
+				error = slc_stty(slc, ifd->ifd_data);
+			break;
+		case SLCGTTY:
+			if (ifd->ifd_len != sizeof(dev_t))
+				error = EINVAL;
+			else
+				error = slc_gtty(slc, ifd->ifd_data);
+			break;
+		default:
+			error = EINVAL;
+			break:
+		}
+		break;
+	case SIOCSIFMTU:
+		if (ifr->ifr_mtu == CAN_MTU) /* XXX */
+			ifp->if_mtu = ifr->ifr_mtu;
+		else
+			error = EINVAL;
+		break;
+	case SIOCSIFFLAGS:
+		break;
+	default:
+		error = EINVAL;
+		break;
+	}
+out:	
+	return (error);
+}
+
+/*
+ * Bottom-level subr.
+ */
+
+static int
+slc_rint(struct tty *tp, char c, int flags)
+{
+	struct slc_softc *slc;
+	struct mbuf *m;
+	int error = 0;
+
+	tty_lock_assert(tp, MA_OWNED);
+
+	if ((slc = ttyhook_softc(tp)) == NULL)
+		goto out;
+
+	mtx_lock(&slc->slc_mtx);
+
+#if 0
+	if (slc->slc_flags & SLC_ERROR) 
+		goto out1; /* XXX */
+#endif 
+
+	/* allocate mbuf(9) and initialize */
+	if ((m = slc->slc_inb) == NULL) {
+		m = m_gethdr(M_NOWAIT, MT_DATA);
+		if (m == NULL) {
+			error = ENOBUFS;
+			goto out1;
+		}
+		m->m_len = m->m_pkthdr.len = 0;
+		slc->slc_inb = m;
+	}
+	
+	*mtod(m, u_char *) = c;
+	
+	m->m_data++;
+	m->m_len++;
+	m->m_pkthdr.len++;
+
+	if (c == SLC_HC_BEL || c == SLC_HC_CR || m->m_len >= SLC_MTU) {
+		m->m_data = m->m_pktdat;
+		error = slc_rxeof(slc);
+	}
+out1:
+	mtx_unlock(&slc->slc_mtx);
+out:
+	return (error);
+}
+
+static size_t
+slc_rint_poll(struct tty *tp)
+{
+	
+	return (1);
+}
+
+static size_t
+slc_txeof(struct tty *tp, void *buf, size_t len)
+{
+	struct slc_softc *slc;
+	struct mbuf *m;
+	size_t off = 0;
+	size_t m_len;
+
+	slc = ttyhook_softc(tp);
+
+	while (len > 0) {
+		IF_DEQUEUE(&slc->slc_outq, m);
+		if (m == NULL)
+			break;
+
+		while (m != NULL) {
+			m_len = min(m->m_len, len);
+			bcopy(mtod(m, caddr_t), (caddr_t)buf + off, m_len);
+
+			m->m_data += m_len;
+			m->m_len -= m_len;
+			off += m_len;
+			len -= m_len;
+
+			if (m->m_len > 0)
+				break;
+			
+			m = m_free(m);
+		}
+
+		if (m != NULL) {
+			IF_PREPEND(&slc->slc_outq, m);
+			break;
+		}
+	}
+	IF_LOCK(&slc->slc_outq);
+	slc->slc_outqlen -= off;
+	IF_UNLOCK(&slc->slc_outq);
+	MPASS(slc->slc_outqlen >= 0);
+
+	return (off);
+}
+
+static size_t
+ngt_txeof_poll(struct tty *tp)
+{
+	struct slc_softc *slc;
+
+	slc = ttyhook_softc(tp);
+
+	return (slc->slc_outqlen);
+}
+
+/*
+ * Subr.
+ */
+
+static void
+slc_destroy(struct slc_softc *slc)
+{
+	struct ifnet *ifp;
+	struct tty *tp;
+	
+	ifp = SLC2IFP(slc);
+	ifp->if_drv_flags &= ~IFF_DRV_RUNNING;
+	ifp->if_flags &= ~IFF_UP;
+
+	can_ifdetach(ifp);
+	if_free(ifp);
+	
+	/* detach hook, if any and flush queue */
+	if ((tp = slc->slc_tp) != NULL) {
+		tty_lock(tp);
+		ttyhook_unregister(tp);
+	}
+	IF_DRAIN(&slc->slc_outq);
+	mtx_destroy(&slc->slc_outq.ifq_mtx);
+	
+	mtx_destroy(&slc->slc_mtx);
+	free(slc, M_SLC);
+}
+
 static int 
 slc_encap(struct slc_softc *slc, struct mbuf **mp)
 {
@@ -385,113 +577,6 @@ slc_encap(struct slc_softc *slc, struct mbuf **mp)
 	return (error);
 }
 
-/*
- * Process an ioctl(2) request.
- */
-/* ARGSUSED */
-static int
-slc_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data)
-{
-	struct ifreq *ifr = (struct ifreq *)data;
-	struct ifdrv *ifd = (struct ifdrv *)data;
-	int error = 0;
-	struct slc_softc *slc;
-
-	switch (cmd) {
-	case SIOCSIFADDR:
-		ifp->if_flags |= IFF_UP;
-		ifp->if_drv_flags |= IFF_DRV_RUNNING;
-		break;
-	case SIOCADDMULTI:
-	case SIOCDELMULTI:
-		if (ifr == NULL) {
-			error = EAFNOSUPPORT;		/* XXX */
-			break;
-		}
-		
-		switch (ifr->ifr_addr.sa_family) {
-#ifdef CAN
-		case AF_CAN:
-			break;
-#endif 	/* CAN */
-		default:
-			error = EAFNOSUPPORT;
-			break;
-		}
-		break;
-	case SIOCGDRVSPEC:
-	case SIOCSDRVSPEC:
-		
-		if ((slc = ifp->if_softc) == NULL) {
-			error = EINVAL;
-			break;
-		}
-
-		switch (ifd->ifd_cmd) {
-		case SLCSTTY:
-			if (ifd->ifd_len != sizeof(int))
-				error = EINVAL;
-			else
-				error = slc_stty(slc, ifd->ifd_data);
-			break;
-		case SLCGTTY:
-			if (ifd->ifd_len != sizeof(dev_t))
-				error = EINVAL;
-			else
-				error = slc_gtty(slc, ifd->ifd_data);
-			break;
-		default:
-			error = EINVAL;
-			break:
-		}
-		break;
-	case SIOCSIFMTU:
-		if (ifr->ifr_mtu == CAN_MTU) /* XXX */
-			ifp->if_mtu = ifr->ifr_mtu;
-		else
-			error = EINVAL;
-		break;
-	case SIOCSIFFLAGS:
-		break;
-	default:
-		error = EINVAL;
-		break;
-	}
-	return (error);
-}
-
-static int 
-slc_stty(struct slc_softc *slc, void *data)
-{
-	int fd = *(int *)data;
-	struct proc *p;
-	int error;
-	
-	mtx_lock(&slc->slc_mtx);
-	
-	if (slc->slc_tp != NULL) {
-		error = EBUSY;
-		goto out;
-	}
-	
-	if ((p = pfind(fd)) == NULL) {
-		error = ESRCH;
-		goto out;
-	}
-	
-	if (p->p_flag & P_WEXIT) {
-		error = ESRCH;
-		goto out;
-	}
-	_PHOLD(p);
-	PROC_UNLOCK(p);
-	error = ttyhook_register(&slc->slc_tp, p, fd, &slc_hook, slc);
-	PRELE(p);	
-out:
-	mtx_unlock(&slc->slc_mtx);
-	return (error);
-}
-
 static int 
 slc_gtty(struct slc_softc *slc, void *data)
 {
@@ -507,56 +592,6 @@ slc_gtty(struct slc_softc *slc, void *data)
 	mtx_unlock(&slc->slc_mtx);
 
 	return (0);
-}
-
-/*
- * Bottom-level subr.
- */
-
-static int
-slc_rint(struct tty *tp, char c, int flags)
-{
-	struct slc_softc *slc;
-	struct mbuf *m;
-	int error = 0;
-
-	tty_lock_assert(tp, MA_OWNED);
-
-	if ((slc = ttyhook_softc(tp)) == NULL)
-		goto out;
-
-	mtx_lock(&slc->slc_mtx);
-
-#if 0
-	if (slc->slc_flags & SLC_ERROR) 
-		goto out1; /* XXX */
-#endif 
-
-	/* allocate mbuf(9) and initialize */
-	if ((m = slc->slc_inb) == NULL) {
-		m = m_gethdr(M_NOWAIT, MT_DATA);
-		if (m == NULL) {
-			error = ENOBUFS;
-			goto out1;
-		}
-		m->m_len = m->m_pkthdr.len = 0;
-		slc->slc_inb = m;
-	}
-	
-	*mtod(m, u_char *) = c;
-	
-	m->m_data++;
-	m->m_len++;
-	m->m_pkthdr.len++;
-
-	if (c == SLC_HC_BEL || c == SLC_HC_CR || m->m_len >= SLC_MTU) {
-		m->m_data = m->m_pktdat;
-		error = slc_rxeof(slc);
-	}
-out1:
-	mtx_unlock(&slc->slc_mtx);
-out:
-	return (error);
 }
 
 static int
@@ -647,65 +682,34 @@ bad:
 	goto out;
 }
 
-static size_t
-slc_rint_poll(struct tty *tp)
+static int 
+slc_stty(struct slc_softc *slc, void *data)
 {
+	int fd = *(int *)data;
+	struct proc *p;
+	int error;
 	
-	return (1);
-}
-
-static size_t
-slc_txeof(struct tty *tp, void *buf, size_t len)
-{
-	struct slc_softc *slc;
-	struct mbuf *m;
-	size_t off = 0;
-	size_t m_len;
-
-	slc = ttyhook_softc(tp);
-
-	while (len > 0) {
-		IF_DEQUEUE(&slc->slc_outq, m);
-		if (m == NULL)
-			break;
-
-		while (m != NULL) {
-			m_len = min(m->m_len, len);
-			bcopy(mtod(m, caddr_t), (caddr_t)buf + off, m_len);
-
-			m->m_data += m_len;
-			m->m_len -= m_len;
-			off += m_len;
-			len -= m_len;
-
-			if (m->m_len > 0)
-				break;
-			
-			m = m_free(m);
-		}
-
-		if (m != NULL) {
-			IF_PREPEND(&slc->slc_outq, m);
-			break;
-		}
+	mtx_lock(&slc->slc_mtx);
+	
+	if (slc->slc_tp != NULL) {
+		error = EBUSY;
+		goto out;
 	}
-	IF_LOCK(&slc->slc_outq);
-	slc->slc_outqlen -= off;
-	IF_UNLOCK(&slc->slc_outq);
-	MPASS(slc->slc_outqlen >= 0);
-
-	return (off);
+	
+	if ((p = pfind(fd)) == NULL) {
+		error = ESRCH;
+		goto out;
+	}
+	
+	if (p->p_flag & P_WEXIT) {
+		error = ESRCH;
+		goto out;
+	}
+	_PHOLD(p);
+	PROC_UNLOCK(p);
+	error = ttyhook_register(&slc->slc_tp, p, fd, &slc_hook, slc);
+	PRELE(p);	
+out:
+	mtx_unlock(&slc->slc_mtx);
+	return (error);
 }
-
-static size_t
-ngt_txeof_poll(struct tty *tp)
-{
-	struct slc_softc *slc;
-
-	slc = ttyhook_softc(tp);
-
-	return (slc->slc_outqlen);
-}
-
-
-
