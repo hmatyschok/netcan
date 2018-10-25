ee1d3cd47ad34fe7b8c9a2e1729e100866862b78
Henning Matyschok <henning.matyschok@stud.fh-flensburg.de>
2018-10-25 21:34:36 +0200

    slcattach(8) merged with module for ifconfig(8).

--- sbin/ifconfig/ifslc.c
+++ sbin/ifconfig/ifslc.c
@@ -53,7 +53,7 @@ slc_status(int s)
 	(void)memset(&ifd, 0, sizeof(ifd));
 	(void)strlcpy(ifd.ifd_name, ifr.ifr_name, sizeof(ifd.ifd_name));
 	
-	ifd.ifd_cmd = IFSLCGTTY;
+	ifd.ifd_cmd = TIOCGETD;
 	ifd.ifd_len = sizeof(tty_dev);
 	ifd.ifd_data = &tty_dev;
 
@@ -66,6 +66,62 @@ slc_status(int s)
 	(void)printf("\tattached: %s\n", devname(tty_dev, S_IFCHR));
 } 
 
+static void
+slc_stty(const char *val, int d, int s, const struct afswtch *afp)
+{
+	char dev_name[MAXPATHLEN];
+	int slc_fd, tty_fd;
+
+	(void)memset(dev_name, 0, sizeof(dev_name));
+	(void)snprintf(dev_name, sizeof(dev_name), "%s%s", 
+		_PATH_DEV, ifr.ifr_name);
+	
+	if ((slc_fd = open(dev_name, O_RDONLY)) < 0)
+		err(1, "cannot open(2) %s", dev_name);	
+
+	(void)memset(dev_name, 0, sizeof(dev_name));
+	(void)snprintf(dev_name, sizeof(dev_name), "%s%s", 
+		_PATH_DEV, val);
+	
+	if ((tty_fd = open(dev_name, O_RDONLY | O_NONBLOCK)) < 0)
+		err(1, "TIOCSETD Can't open %s device", dev_name); 	
+	
+	if (isatty(tty_fd) == 0)
+		err(1, "TIOCSETD %s not a terminal type device", dev_name);
+	
+	if (ioctl(slc_dev, TIOCSETD, &tty_fd) < 0)
+		err(1, "TIOCSETD Can't attach %s to %s", 
+			dev_name, ifr.ifr_name);
+			
+	(void)close(tty_fd);
+	(void)close(slc_fd);
+}
+
+static void
+slc_dtty(const char *val, int d, int s, const struct afswtch *afp)
+{
+	char dev_name[MAXPATHLEN];
+	int slc_fd;
+
+	(void)memset(dev_name, 0, sizeof(dev_name));
+	(void)snprintf(dev_name, sizeof(dev_name), "%s%s", 
+		_PATH_DEV, ifr.ifr_name);
+	
+	if ((slc_fd = open(dev_name, O_RDONLY)) < 0)
+		err(1, "cannot open(2) %s", dev_name);	
+
+	if (ioctl(slc_dev, TIOCNOTTY, &tty_fd) < 0)
+		err(1, "TIOCSETD Can't attach %s to %s", 
+			dev_name, ifr.ifr_name);
+
+	(void)close(slc_fd);
+}
+
+static struct cmd slc_cmds[] = {
+	DEF_CMD_ARG("stty",		slc_stty),
+	DEF_CMD("dtty", 0,		slc_dtty),
+};
+
 static struct afswtch af_slc = {
 	.af_name	= "af_slc",
 	.af_af		= AF_UNSPEC,
@@ -75,6 +131,9 @@ static struct afswtch af_slc = {
 static __constructor void
 slc_ctor(void)
 {
-	
+	int i;
+
+	for (i = 0; i < nitems(slc_cmds);  i++)
+		cmd_register(&slc_cmds[i]);
 	af_register(&af_slc);
 }
