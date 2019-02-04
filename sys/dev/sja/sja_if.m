df025078cd02314ccd5a773d481f9df01b885aa3
Henning Matyschok <henning.matyschok@stud.fh-flensburg.de>
2019-02-04 19:26:11 +0100

    I/O on sja(4).

--- sys/dev/sja/sja_if.m
+++ sys/dev/sja/sja_if.m
@@ -27,12 +27,51 @@
 
 #include <sys/bus.h>
 
-INTERFACE c_can;
+INTERFACE sja;
 
 #
 # Read register.
 #
-METHOD uint32_t readreg {
+METHOD uint8_t read_1 {
+	device_t		dev;
+	uint32_t		port;
+};
+
+METHOD uint16_t read_2 {
+	device_t		dev;
+	uint32_t		port;
+};
+
+METHOD uint32_t read_4 {
+	device_t		dev;
+	uint32_t		port;
+};
+
+#
+# Write register.
+#
+METHOD uint8_t write_1 {
+	device_t		dev;
+	uint32_t		port;
+	uint32_t		val;
+};
+
+METHOD uint16_t write_2 {
+	device_t		dev;
+	uint32_t		port;
+	uint32_t		val;
+};
+
+METHOD uint32_t write_4 {
+	device_t		dev;
+	uint32_t		port;
+	uint32_t		val;
+};
+
+#
+# Post interrupt handling.
+#
+METHOD void clear_intr {
 	device_t		dev;
 	uint32_t 	port;
 };
