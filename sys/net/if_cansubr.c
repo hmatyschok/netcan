79d4a51a79ac2b92e24b2eca51a5dc7ef3aab04d
Henning Matyschok <henning.matyschok@stud.fh-flensburg.de>
2019-03-05 14:47:18 +0100

    Typos.

--- sys/net/if_cansubr.c
+++ sys/net/if_cansubr.c
@@ -283,7 +283,8 @@ can_ifattach(struct ifnet *ifp, struct can_link_timecaps *cltc,
 	mtx_init(&csc->csc_mtx, "csc_mtx", NULL, MTX_DEF);
 	
 	if (cltc != NULL) {
-		bcopy(cltc, &csc->csc_timecaps, sizeof(*cltc));
+		bcopy(cltc, &csc->csc_timecaps, 
+			sizeof(struct can_link_timecaps));
 		csc->csc_timecaps.cltc_clock_freq = freq;
 	}
 	
