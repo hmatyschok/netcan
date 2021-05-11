b1187699b90f9cce56331c2217c6f85609f084f1
Henning Matyschok <henning.matyschok@outlook.com>
2021-05-11 10:06:38 +0200

    Cleanup.

--- sys/net/if_can.h
+++ sys/net/if_can.h
@@ -64,7 +64,6 @@
  */
 /*
  * Copyright (c) 2018, 2019 Henning Matyschok
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
